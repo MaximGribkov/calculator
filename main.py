c0db7a76ca33b916b2dd683cf2cbad2883cecd75
MVGribkov <ffzzsshh@gmail.com>
2022-05-20 16:49:23 +0300

    Added several functions

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ class calc(QtWidgets.QWidget):
         self.setWindowTitle('calculator')
         self.my_layout = QtWidgets.QGridLayout()
         self.setLayout(self.my_layout)
+        self.value = ''
+        self.operator = ''
+        self.num = 0
 
         self.btn_0 = QtWidgets.QPushButton('0')
         self.btn_1 = QtWidgets.QPushButton('1')
@@ -53,10 +56,163 @@ class calc(QtWidgets.QWidget):
         self.my_layout.addWidget(self.btn_sign, 5, 2)
         self.my_layout.addWidget(self.btn_degree, 1, 0)
 
+        self.btn_1.clicked.connect(self.one)
+        self.btn_2.clicked.connect(self.two)
+        self.btn_3.clicked.connect(self.three)
+        self.btn_4.clicked.connect(self.four)
+        self.btn_5.clicked.connect(self.five)
+        self.btn_6.clicked.connect(self.six)
+        self.btn_7.clicked.connect(self.seven)
+        self.btn_8.clicked.connect(self.eight)
+        self.btn_9.clicked.connect(self.nine)
+        self.btn_0.clicked.connect(self.zero)
+        self.btn_plus.clicked.connect(self.plus)
+        self.btn_minus.clicked.connect(self.minus)
+        self.btn_clean.clicked.connect(self.clean)
+        self.btn_multiply.clicked.connect(self.multiply)
+        self.btn_divide.clicked.connect(self.divide)
+        self.btn_percent.clicked.connect(self.percent)
+        self.btn_equals.clicked.connect(self.equals)
+        self.btn_dot.clicked.connect(self.dot)
+        self.btn_sign.clicked.connect(self.sign)
+        self.btn_degree.clicked.connect(self.degree)
 
+    def plus(self):
+        self.num = int(self.value)
+        self.operator = '+'
+        self.value = self.value + '+'
+        self.answer.setText('{}'.format(self.value))
 
+    def minus(self):
+        self.num = int(self.value)
+        self.operator = '-'
+        self.value = self.value + '-'
+        self.answer.setText('{}'.format(self.value))
 
+    def clean(self):
+        self.num = 0
+        self.operator = ''
+        self.value = ''
+        self.answer.setText('{}'.format(self.value))
 
+    def multiply(self):
+        self.num = int(self.value)
+        self.operator = '*'
+        self.value = self.value + '*'
+        self.answer.setText('{}'.format(self.value))
+
+    def divide(self):
+        self.num = int(self.value)
+        self.operator = '/'
+        self.value = self.value + '/'
+        self.answer.setText('{}'.format(self.value))
+
+    def percent(self):
+        self.num = int(self.value)
+        self.operator = '%'
+        self.value = self.value + '%'
+        self.answer.setText('{}'.format(self.value))
+
+    def degree(self):
+        self.num = int(self.value)
+        self.operator = '^'
+        self.value = self.value + '^'
+        self.answer.setText('{}'.format(self.value))
+
+    def sign(self):'''
+        self.num = int(self.val)
+        self.operator = '+'
+        self.val = self.val + '+'
+        self.answer.setText('{}'.format(self.val))'''
+
+    def dot(self):
+        self.num = float(self.value)
+        self.value = self.value + '.'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def one(self):
+        self.value = self.value + '1'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def two(self):
+        self.value = self.value + '2'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def three(self):
+        self.value = self.value + '3'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def four(self):
+        self.value = self.value + '4'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def five(self):
+        self.value = self.value + '5'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def six(self):
+        self.value = self.value + '6'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def seven(self):
+        self.value = self.value + '7'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def eight(self):
+        self.value = self.value + '8'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def nine(self):
+        self.value = self.value + '9'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def zero(self):
+        self.value = self.value + '0'
+        self.answer.setText('{}'.format(self.value))
+        print(self.value)
+
+    def equals(self):
+        value_2 = self.value
+        if self.operator == '+':
+            x = int((value_2.split('+')[1]))
+            y = self.num + x
+            self.answer.setText('{}'.format(y))
+            self.value = str(y)
+        elif self.operator == '-':
+            x = int((value_2.split('-')[1]))
+            y = self.num - x
+            self.answer.setText('{}'.format(y))
+            self.value = str(y)
+        elif self.operator == '*':
+            x = int((value_2.split('*')[1]))
+            y = self.num * x
+            self.answer.setText('{}'.format(y))
+            self.value = str(y)
+        elif self.operator == '^':
+            x = int((value_2.split('^')[1]))
+            y = self.num ** x
+            self.answer.setText('{}'.format(y))
+            self.value = str(y)
+        elif self.operator == '/':
+            try:
+                x = int((value_2.split('/')[1]))
+                y = self.num / x
+                self.answer.setText('{}'.format(y))
+                self.value = str(y)
+            except ZeroDivisionError:
+                self.answer.setText('Division by 0 not allowed!')
+                self.num = ''
+                self.value = ''
 
 
 if __name__ == "__main__":
