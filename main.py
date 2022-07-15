7354cb813059abea87b11c1727dc0cecdbe58e1f
MVGribkov <ffzzsshh@gmail.com>
2022-07-15 16:53:25 +0300

    Added some functionality and added a few comments

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ operation = {
 dev_zero_error = 'Division by zero'
 undefined_error = 'Result is undefined'
 
+size_label = 15
+size_lineedit = 45
+
 
 class calc(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -22,6 +25,7 @@ class calc(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.setupUi(self)
         self.max_len = self.ui.lineEdit.maxLength()
 
+        # Add buttons
         self.ui.btn_1.clicked.connect(self.add_digit)
         self.ui.btn_2.clicked.connect(self.add_digit)
         self.ui.btn_3.clicked.connect(self.add_digit)
@@ -33,12 +37,13 @@ class calc(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.btn_9.clicked.connect(self.add_digit)
         self.ui.btn_0.clicked.connect(self.add_digit)
 
+        # Del button
         self.ui.btn_clear.clicked.connect(self.clean_all)
         self.ui.btn_del_one_element.clicked.connect(self.backspace)
 
+        # Math operations
         self.ui.btn_dot.clicked.connect(self.dot)
         self.ui.btn_sign.clicked.connect(self.negation)
-
         self.ui.btn_plus.clicked.connect(self.math_operations)
         self.ui.btn_minus.clicked.connect(self.math_operations)
         self.ui.btn_multiply.clicked.connect(self.math_operations)
@@ -46,7 +51,7 @@ class calc(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.btn_degree.clicked.connect(self.math_operations)
         self.ui.btn_equals.clicked.connect(self.calculate)
 
-    def add_digit(self):
+    def add_digit(self):  # Add num
         btn = self.sender()
         self.del_error()
         self.clear_label_equals()
@@ -58,12 +63,15 @@ class calc(QtWidgets.QMainWindow, Ui_MainWindow):
             else:
                 self.ui.lineEdit.setText(self.ui.lineEdit.text() + btn.text())
 
-    def clean_all(self):
+            self.adjust_lineedit()
+
+    def clean_all(self):  # Delete all
         self.del_error()
         self.ui.lineEdit.setText('0')
         self.ui.label.clear()
+        self.adjust_lineedit()
 
-    def backspace(self):
+    def backspace(self):  # Delete all
         self.del_error()
         entry = self.ui.lineEdit.text()
         if len(entry) != 1:
@@ -73,38 +81,41 @@ class calc(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.ui.lineEdit.setText(entry[:-1])
         else:
             self.ui.lineEdit.setText('0')
+            self.adjust_lineedit()
 
     def dot(self):
         self.clear_label_equals()
         if '.' not in self.ui.lineEdit.text():
             self.ui.lineEdit.setText(self.ui.lineEdit.text() + '.')
+            self.adjust_lineedit()
 
-    def add_mem(self):
+    def add_label(self):  # Label output
         btn = self.sender()
         entry = self.del_zero(self.ui.lineEdit.text())
         if not self.ui.label.text() or self.get_sign() == '=':
             self.ui.label.setText(entry + f' {btn.text()}')
             self.ui.lineEdit.setText('0')
+            self.adjust_lineedit()
 
     @staticmethod
-    def del_zero(num):
+    def del_zero(num):  # Clear up extra zeros
         n = str(float(num))
         return n[:-2] if n[-2:] == '.0' else n
 
-    def get_num_lineedit(self):
+    def get_num_lineedit(self):  # Get num from lineedit
         entry = self.ui.lineEdit.text().strip('.')
         return float(entry) if '.' in entry else int(entry)
 
-    def get_num_label(self):
+    def get_num_label(self):  # Get num from label
         if self.ui.label.text():
             temp = self.ui.label.text().strip('.').split()[0]
             return float(temp) if '.' in temp else int(temp)
 
-    def get_sign(self):
+    def get_sign(self):  # Get math sign
         if self.ui.label.text():
             return self.ui.label.text().strip('.').split()[-1]
 
-    def calculate(self):
+    def calculate(self):  # Calculation
         entry = self.ui.lineEdit.text()
         temp = self.ui.label.text()
         if temp:
@@ -121,16 +132,17 @@ class calc(QtWidgets.QMainWindow, Ui_MainWindow):
                     self.show_error(undefined_error)
                 else:
                     self.show_error(dev_zero_error)
+            self.adjust_lineedit()
 
-    def math_operations(self):
+    def math_operations(self):  # Math operation
         temp = self.ui.label.text()
         btn = self.sender()
         if not temp:
-            self.add_mem()
+            self.add_label()
         else:
             if self.get_sign() != btn.text():
                 if self.get_sign() == '=':
-                    self.add_mem()
+                    self.add_label()
                 else:
                     self.ui.label.setText(temp[:-2] + f'{btn.text()}')
             else:
@@ -138,8 +150,9 @@ class calc(QtWidgets.QMainWindow, Ui_MainWindow):
                     self.ui.label.setText(self.calculate() + f'{btn.text()}')
                 except TypeError:
                     pass
+        self.adjust_lineedit()
 
-    def negation(self):
+    def negation(self):  # Negation
         entry = self.ui.lineEdit.text()
         self.clear_label_equals()
         if '-' not in entry:
@@ -152,19 +165,35 @@ class calc(QtWidgets.QMainWindow, Ui_MainWindow):
         else:
             self.ui.lineEdit.setMaxLength(self.max_len)
         self.ui.lineEdit.setText(entry)
+        self.adjust_lineedit()
 
-    def clear_label_equals(self):
+    def clear_label_equals(self):  # Clear label
         if self.get_sign() == '=':
             self.ui.label.clear()
 
-    def show_error(self, text):
+    def show_error(self, text):  # Show error in lineedit
         self.ui.lineEdit.setMaxLength(len(text))
         self.ui.lineEdit.setText(text)
 
-    def del_error(self):
+    def del_error(self):  # Del error in lineedit
         if self.ui.lineEdit.text() in (dev_zero_error, undefined_error):
             self.ui.lineEdit.setMaxLength(self.max_len)
             self.ui.lineEdit.setText('0')
+
+    def get_lineedit_width(self):  # Width lineedit
+        return self.ui.lineEdit.fontMetrics().boundingRect(self.ui.lineEdit.text()).width()
+
+    def adjust_lineedit(self):  # Decrease or increase the front in lineedit
+        font_size = size_lineedit
+        while self.get_lineedit_width() > self.ui.lineEdit.width() - 15:
+            font_size -= 1
+            self.ui.lineEdit.setStyleSheet('font-size: ' + str(font_size) + 'pt; border: none;')
+        font_size = 1
+        while self.get_lineedit_width() < self.ui.lineEdit.width() - 65:
+            font_size += 1
+            if font_size > size_lineedit:
+                break
+            self.ui.lineEdit.setStyleSheet('font-size: ' + str(font_size) + 'pt; border: none;')
 
 
 if __name__ == "__main__":
